7ca538a4afc5d330c1c88ad42e2f366d37e823b0
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-06 16:23:38 +0530

    Add configuration for Appwrite API in config.js

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,3 +1,9 @@
-const conf = {};
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_API_ID),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+};
 
 export default conf;
